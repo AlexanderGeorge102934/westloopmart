c42ef1597253ce4bec6933a96706894192840e64
alexGeorge-cyber <ageorg48@uic.edu>
2024-06-01 14:43:30 -0500

    Added helpers

--- lib/helpers/helpers.dart
+++ lib/helpers/helpers.dart
@@ -1,3 +1,38 @@
 //Lot of useful helper functions in the video but too much to implement
 
 //Mastering Flutter Essential Utilities helper functions constants and much more
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+class THelperFunctions{
+
+  static double screenWidth(){
+    return MediaQuery.of(Get.context!).size.width;
+  }
+
+  static void navigateToScreen(BuildContext context, Widget screen){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_)=> screen),
+    );
+  } //Check to see if this works
+
+  static bool isDarkMode(BuildContext context){
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  static Size screenSize(){
+    return MediaQuery.of(Get.context!).size;
+  }
+
+  static double screenHeight(){
+    return MediaQuery.of(Get.context!).size.height;
+  }
+
+  static List<T> removeDuplicates<T>(List<T> list){
+    return list.toSet().toList();
+  }
+
+
+}
