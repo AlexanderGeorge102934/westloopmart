48ccf15627f11c17f64ab4175db6795354b34903
alexGeorge-cyber <ageorg48@uic.edu>
2024-07-05 20:00:13 -0500

    Cleaned up post screen moved post controller outside of the build method

--- lib/features/authentication/screens/add_post/add_post.dart
+++ lib/features/authentication/screens/add_post/add_post.dart
@@ -1,14 +1,8 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:startup_app/helpers/helpers.dart';
-import '../../../../common/styles/spacing_styles.dart';
 import '../../../../utils/constants/colors.dart';
-import '../../../../utils/constants/sizes.dart';
-import 'Widgets/image_grid.dart';
 import '../../controllers/add_post/add_post_controller.dart';
-import 'package:permission_handler/permission_handler.dart';
-import '../../controllers/camera/camera_controller.dart';
 import '../../controllers/images/image_controller.dart';
 import 'Widgets/add_post_form.dart';
 
@@ -16,11 +10,12 @@ import 'Widgets/add_post_form.dart';
 class AddPostScreen extends StatelessWidget {
   AddPostScreen({super.key});
   final ImageController imageController = Get.put(ImageController());
+  final PostController postController = Get.put(PostController());
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    final postController = Get.put(PostController());
+
 
 
     return Scaffold(
