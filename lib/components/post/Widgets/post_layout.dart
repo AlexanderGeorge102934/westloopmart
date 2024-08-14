5c9e643d9eee2736c3dc5ca3372d8107f0864f6f
alexGeorge-cyber <ageorg48@uic.edu>
2024-08-14 12:05:23 -0500

    Changed UI of post a bit

--- lib/components/post/Widgets/post_layout.dart
+++ lib/components/post/Widgets/post_layout.dart
@@ -1,14 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-
+import 'package:geolocator/geolocator.dart';
 import '../../../features/authentication/controllers/image_carousel/image_carousel_controller.dart';
 import '../../../features/authentication/screens/add_offer/add_offer.dart';
 import '../../../features/authentication/screens/offers/offers_screen.dart';
-import '../../../helpers/helpers.dart';
-import '../../../utils/constants/sizes.dart';
 import '../../../utils/constants/texts.dart';
 import '../../offer/Widgets/image_carousel.dart';
 
@@ -36,37 +31,104 @@ class TPostLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-
-        /// Image Carousel
-        TImageCarousel(controller: controller, imageUrls: imageUrls),
-        SizedBox(height: TSizes.spaceBtwItems(context)),
-
-        /// Title
-        Text(title,
-          style: Theme.of(context).textTheme.bodyLarge,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              spreadRadius: 2,
+              offset: Offset(0, 4),
+            ),
+          ],
         ),
-
-        ///Name
-        Text(user,
-          style: Theme.of(context).textTheme.bodySmall,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TImageCarousel(controller: controller, imageUrls: imageUrls),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    user,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                  Text(
+                    distanceString,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () => Get.to(() => AddOfferScreen(
+                        postID: postID,
+                        userOfPost: user,
+                        titleOfPost: title,
+                        userOfPostId: userId,
+                      )),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blueAccent,
+                        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        elevation: 5,
+                        textStyle: TextStyle(fontSize: 16),
+                      ),
+                      child: const Text(TTexts.offerPost),
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () => Get.to(() => OffersScreen(
+                        postID: postID,
+                        userPosition: userPosition,
+                        userId: userId,
+                      )),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.greenAccent,
+                        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        elevation: 5,
+                        textStyle: TextStyle(fontSize: 16),
+                      ),
+                      child: const Text('Show Offers'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-
-        /// distance
-        Text(distanceString,
-          style: Theme.of(context).textTheme.bodySmall,
-        ),
-
-        /// Add offer button
-        Center(child: SizedBox(width: THelperFunctions.screenWidth() * 0.7, child: ElevatedButton(onPressed: ()=> Get.to(()=> AddOfferScreen(postID: postID, userOfPost: user, titleOfPost: title, userOfPostId: userId)), child: const Text(TTexts.offerPost)))),
-        SizedBox(height: TSizes.spaceBtwItems(context)),
-
-        /// See offers button
-        Center(child: ElevatedButton(onPressed: ()=> Get.to(()=>OffersScreen(postID: postID, userPosition: userPosition, userId: userId)),child: const Text('Show Offers'))),
-        SizedBox(height: TSizes.spaceBtwSections(context)),
-      ],
+      ),
     );
   }
 }
